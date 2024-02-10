83a4ceafa6f6814db051d8dcd7fca0447b133d08
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-02-10 15:07:12 -0300

    Bookshelf: add BookCard ui item

--- Bookshelf/app/build.gradle.kts
+++ Bookshelf/app/build.gradle.kts
@@ -68,5 +68,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     //Coil
-    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
