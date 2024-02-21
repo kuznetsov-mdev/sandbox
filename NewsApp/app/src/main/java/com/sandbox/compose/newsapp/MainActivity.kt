ffa1db873d887bb61ebb54ff5b05f23935bed386
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-02-21 17:51:45 -0300

    NewsApp-2: clear main activity

--- NewsApp/app/src/main/java/com/sandbox/compose/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/MainActivity.kt
@@ -3,13 +3,6 @@ package com.sandbox.compose.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sandbox.compose.newsapp.ui.theme.NewsAppTheme
 
@@ -19,27 +12,8 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppTheme {
-                installSplashScreen()
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppTheme {
-        Greeting("Android")
     }
 }
