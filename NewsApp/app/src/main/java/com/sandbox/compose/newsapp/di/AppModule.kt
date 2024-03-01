2c47c43f3455a7f020d00fdcc62b24c72495a69c
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-01 17:46:48 -0300

    NewsApp-5: add di package

---
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.sandbox.compose.newsapp.di
+
+import android.app.Application
+import com.sandbox.compose.newsapp.data.manager.LocalUserManagerImpl
+import com.sandbox.compose.newsapp.domain.manager.LocalUserManager
+import com.sandbox.compose.newsapp.domain.usecases.AppEntryUseCases
+import com.sandbox.compose.newsapp.domain.usecases.ReadAppEntry
+import com.sandbox.compose.newsapp.domain.usecases.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(
+        context: Application
+    ): LocalUserManager = LocalUserManagerImpl(context)
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCases(userManager: LocalUserManager): AppEntryUseCases {
+        return AppEntryUseCases(
+            readAppEntry = ReadAppEntry(userManager),
+            saveAppEntry = SaveAppEntry(userManager)
+        )
+    }
+}
