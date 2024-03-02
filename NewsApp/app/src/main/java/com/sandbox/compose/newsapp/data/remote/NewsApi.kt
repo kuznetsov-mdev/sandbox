2721f1676d5f865bf2dcd8cf5de621293b154862
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-02 17:09:53 -0300

    NewsApp-9: add NewsApi retrofit interphase

---
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/data/remote/NewsApi.kt
@@ -0,0 +1,15 @@
+package com.sandbox.compose.newsapp.data.remote
+
+import com.sandbox.compose.newsapp.data.remote.dto.NewsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("everything")
+    suspend fun getNews(
+        @Query("page") page: Int,
+        @Query("sources") sources: String,
+        @Query("apiKey") apiKey: String
+    ): NewsResponse
+}
