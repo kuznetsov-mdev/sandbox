677cc7e524591306e3d7e516e5761d5e0bd25526
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-02 15:09:56 -0300

    NewsApp-7: add navigation routes

---
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/presentation/navgraph/NavGraph.kt
@@ -0,0 +1,43 @@
+package com.sandbox.compose.newsapp.presentation.navgraph
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.sandbox.compose.newsapp.presentation.onboarding.OnBoardingScreen
+import com.sandbox.compose.newsapp.presentation.onboarding.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ) {
+            composable(route = Route.OnBoardingScreen.route) {
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(event = viewModel::onEvent)
+            }
+        }
+
+        //subgraph
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.NewsNavigatorScreen.route
+        ) {
+            composable(route = Route.NewsNavigatorScreen.route) {
+                Text(text = "News Navigator Screen")
+            }
+        }
+    }
+}
