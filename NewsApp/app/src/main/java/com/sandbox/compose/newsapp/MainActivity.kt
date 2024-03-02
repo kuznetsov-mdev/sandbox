6bb3f54377affd42e52cd05c3934c501f8c11e3d
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-02 13:10:43 -0300

    NewsApp-6: remove test Dagger Hilt DI

--- NewsApp/app/src/main/java/com/sandbox/compose/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.sandbox.compose.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,30 +9,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import com.sandbox.compose.newsapp.domain.usecases.AppEntryUseCases
 import com.sandbox.compose.newsapp.presentation.onboarding.OnBoardingScreen
 import com.sandbox.compose.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
-        lifecycleScope.launch {
-            appEntryUseCases.readAppEntry().collectLatest {
-                Log.d("Test Dagger DI", it.toString())
-            }
-        }
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
