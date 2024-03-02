31a749245425d541a92ddaf77e27346dcdfe5979
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-02 15:40:25 -0300

    NewsApp-7: add navigation from OnBoardingScreen to NewNavigatorScreen

--- NewsApp/app/src/main/java/com/sandbox/compose/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/MainActivity.kt
@@ -3,30 +3,34 @@ package com.sandbox.compose.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.sandbox.compose.newsapp.presentation.onboarding.OnBoardingScreen
-import com.sandbox.compose.newsapp.presentation.onboarding.OnBoardingViewModel
+import com.sandbox.compose.newsapp.presentation.navgraph.NavGraph
 import com.sandbox.compose.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
+            }
+        }
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(event = viewModel::onEvent)
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
