716841a2cad7ab01fc134015533010fded0666bb
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-03 20:01:40 -0300

    NewsApp-10: add HomeScreen

---
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,56 @@
+package com.sandbox.compose.newsapp.presentation.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import com.sandbox.compose.newsapp.R
+import com.sandbox.compose.newsapp.domain.model.Article
+import com.sandbox.compose.newsapp.presentation.Dimens.MediumPadding_24
+
+@Composable
+fun HomeScreen(
+    articles: LazyPagingItems<Article>,
+    navigate: (String) -> Unit
+) {
+    val titles by remember {
+        derivedStateOf {
+            if (articles.itemCount > 10) {
+                articles.itemSnapshotList.items
+                    .slice(IntRange(start = 0, endInclusive = 9))
+                    .joinToString(separator = " \uD83d\uDFE5 ") { it.title }
+            } else {
+                ""
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(MediumPadding_24)
+            .statusBarsPadding()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier
+                .width(150.dp)
+                .height(30.dp)
+                .padding(horizontal = MediumPadding_24)
+        )
+    }
+    Spacer(modifier = Modifier.height(MediumPadding_24))
+}
