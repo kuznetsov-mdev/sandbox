e3a2b6a11507ec0674bbabdc92e87f5c26b3feef
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-04 16:19:41 -0300

    NewsApp-11: add searchNews method to NewsRepository

--- NewsApp/app/src/main/java/com/sandbox/compose/newsapp/data/repository/NewsRepositoryImpl.kt
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/data/repository/NewsRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.sandbox.compose.newsapp.data.remote.NewsApi
 import com.sandbox.compose.newsapp.data.remote.NewsPagingSource
+import com.sandbox.compose.newsapp.data.remote.SearchNewsPagingSource
 import com.sandbox.compose.newsapp.domain.model.Article
 import com.sandbox.compose.newsapp.domain.repository.NewsRepositoryApi
 import kotlinx.coroutines.flow.Flow
@@ -18,6 +19,19 @@ class NewsRepositoryImpl(
             config = PagingConfig(pageSize = 10),
             pagingSourceFactory = {
                 NewsPagingSource(
+                    newsApi = newsApi,
+                    sources = sources.joinToString(",")
+                )
+            }
+        ).flow
+    }
+
+    override fun searchNews(searchQuery: String, sources: List<String>): Flow<PagingData<Article>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                SearchNewsPagingSource(
+                    searchQuery = searchQuery,
                     newsApi = newsApi,
                     sources = sources.joinToString(",")
                 )
