50a68e6b23169fb4a6f0f8bbdef593bd159940ac
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-06 16:11:05 -0300

    NewsApp-14: add Entities

--- NewsApp/app/src/main/java/com/sandbox/compose/newsapp/domain/model/Article.kt
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/domain/model/Article.kt
@@ -1,5 +1,14 @@
 package com.sandbox.compose.newsapp.domain.model
 
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+//Todo: create separete models for ui, network, db
+
+@Parcelize
+@Entity
 data class Article(
     val author: String,
     val content: String,
@@ -7,6 +16,6 @@ data class Article(
     val publishedAt: String,
     val source: Source,
     val title: String,
-    val url: String,
+    @PrimaryKey val url: String,
     val urlToImage: String
-)
+) : Parcelable
