b89c55fc3c90baa75179805b3fa41844fb337622
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2024-03-06 16:16:19 -0300

    NewsApp-14: add TypeConverter for source

--- NewsApp/app/src/main/java/com/sandbox/compose/newsapp/data/local/NewsTypeConverter.kt
+++ NewsApp/app/src/main/java/com/sandbox/compose/newsapp/data/local/NewsTypeConverter.kt
@@ -1,8 +1,22 @@
 package com.sandbox.compose.newsapp.data.local
 
 import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.sandbox.compose.newsapp.domain.model.Source
 
 @ProvidedTypeConverter
 class NewsTypeConverter {
 
+    @TypeConverter
+    fun sourceToString(source: Source): String {
+        return "${source.id},${source.name}"
+    }
+
+    @TypeConverter
+    fun stringToSource(string: String): Source {
+        return Source(
+            id = string.split(",")[0],
+            name = string.split(",")[1]
+        )
+    }
 }
